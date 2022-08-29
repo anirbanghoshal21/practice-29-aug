d8b399db99a5899c9821527b57e62a282afb89ff
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-29 20:30:18 +0530

    Add files via upload

---
+++ Doubly linked list.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_implement
+{
+    class LinkedList
+    {
+        
+            internal DoublyLinkedList head;
+            void InsertNodeInFront(UserDoubleLinkedList list, int dataToInsert)
+            {
+                DoublyLinkedList node = new DoublyLinkedList(dataToInsert);
+                node.next = list.head;
+                list.head = node;
+                node.prev = null;
+                if (list.head != null)
+                {
+                    list.head.prev = node;
+                }
+
+
+            }
+
+
+
+            void InsertAfter(DoublyLinkedList prevnode, int data)
+            {
+                if (prevnode == null)
+                {
+                    Console.WriteLine("cannot be null");
+                    return;
+
+                }
+
+
+                DoublyLinkedList newnode = new DoublyLinkedList(data);
+                newnode.next = prevnode.next;
+                prevnode.next = newnode;
+                newnode.prev = prevnode;
+                if (newnode.next != null)
+                {
+                    newnode.next.prev = newnode;
+                }
+
+            }
+            void InsertAtTheEnd(DoublyLinkedList list, int dataToInsert)
+            {
+                DoublyLinkedList node = new DoublyLinkedList(dataToInsert);
+                if (list.head == null)
+                {
+                    list.prev = null;
+                    list.head = node;
+                    return;
+                }
+                DoublyLinkedListNodeDemo node1 = GetLastNode(list);
+                node1.next = node;
+                node.prev = node1;
+
+            }
+
+            private DoublyLinkedList GetLastNode(DoublyLinkedList list)
+            {
+                DoublyLinkedList tempnode = list.head;
+                while (tempnode != null)
+                {
+                    tempnode = tempnode.next;
+
+                }
+                return tempnode;
+            }
+}
